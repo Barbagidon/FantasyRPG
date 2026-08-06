8b04859e214ea82e51943aeb8a0710c9d245977d
dshtiller <dshtiller@ourgapps.com>
2026-08-07 00:27:57 +0300

    test: add GridPosition Equals coverage (same/different coords, object overload)
    Closes part of the Milestone-1 debt: GridPosition/CombatMap shipped in 68ecb6d
    with zero test coverage. GetHashCode contract test and CombatMapTests still
    pending.

---
+++ Assets/Tests/Core/GridPositionTests.cs
@@ -0,0 +1,40 @@
+using FantasyRPG.Core.Combat;
+using NUnit.Framework;
+
+namespace FantasyRPG.Core.Tests
+{
+    [TestFixture]
+    public class GridPositionTests
+    {
+        [Test]
+        public void Equals_SameCoordinates_ReturnsTrue()
+        {
+            GridPosition firstPosition = new(1, 2);
+            GridPosition secondPosition = new(1, 2);
+
+            bool areEqual = firstPosition.Equals(secondPosition);
+            Assert.IsTrue(areEqual);
+        }
+
+        [Test]
+        public void Equals_DifferentCoordinates_ReturnsFalse()
+        {
+            GridPosition firstPosition = new(1, 1);
+            GridPosition secondPosition = new(1, 2);
+
+            bool areEqual = firstPosition.Equals(secondPosition);
+            Assert.IsFalse(areEqual);
+        }
+
+        [Test]
+        public void Equals_ObjectOverloadWithSameCoordinates_ReturnsTrue()
+        {
+            GridPosition firstPosition = new(1, 2);
+            GridPosition secondPosition = new(1, 2);
+
+            bool areEqual = firstPosition.Equals((object)secondPosition);
+            Assert.IsTrue(areEqual);
+        }
+    }
+}
+
